03e095d7233db7dde5acd8990dc56eda6de301b7
Alex Wall <alex@noligy.de>
2010-10-27 11:30:06 +0200

    spike: md5 hash of text file

--- hashes/hashes.js
+++ hashes/hashes.js
@@ -5,3 +5,4 @@ var hash = require('hashlib');
 var text = "Create a md5 hash";
 
 console.log('"' + text + '" => ' + hash.md5(text));
+console.log('MD5 of file "text.txt" => ' + hash.md5_file('text.txt'));
